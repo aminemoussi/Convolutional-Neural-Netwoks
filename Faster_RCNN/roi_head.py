0aef3bdc23e68a5bfe7c0aaf5c8d87d08953043b
Amnie <aminemandm@gmail.com>
2025-10-03 23:24:07 +0200

    assign_target_to_proposals in roi_head

--- Faster_RCNN/roi_head.py
+++ Faster_RCNN/roi_head.py
@@ -27,22 +27,46 @@ class ROIHead(nn.Module):
         self.cls_layer = nn.Linear(self.fc_inner_dim, self.num_classes)
         self.bbox_reg_layer = nn.Linear(self.fc_inner_dim, self.num_classes * 4)
 
-    def assign_target_to_proposal(self, proposals, gt_boxes, gt_lables):
-        iou_matrix = core.iou(gt_boxes, proposals)
-        # best match per proposal
-        iou_best_match, best_gt_index = iou_matrix.max(dim=0)
+    def assign_target_to_proposals(self, proposals, gt_boxes, gt_labels):
+        r"""
+        Given a set of proposals and ground truth boxes and their respective labels.
+        Use IOU to assign these proposals to some gt box or background
+        :param proposals: (number_of_proposals, 4)
+        :param gt_boxes: (number_of_gt_boxes, 4)
+        :param gt_labels: (number_of_gt_boxes)
+        :return:
+            labels: (number_of_proposals)
+            matched_gt_boxes: (number_of_proposals, 4)
+        """
+        # Get IOU Matrix between gt boxes and proposals
+        iou_matrix = get_iou(gt_boxes, proposals)
+        # For each gt box proposal find best matching gt box
+        best_match_iou, best_match_gt_idx = iou_matrix.max(dim=0)
+        background_proposals = (best_match_iou < self.iou_threshold) & (
+            best_match_iou >= self.low_bg_iou
+        )
+        ignored_proposals = best_match_iou < self.low_bg_iou
 
-        below_low_thrshold = iou_best_match < 0.5
-        best_gt_index[below_low_thrshold] = -1
+        # Update best match of low IOU proposals to -1
+        best_match_gt_idx[background_proposals] = -1
+        best_match_gt_idx[ignored_proposals] = -2
 
-        matched_gt_boxes_per_proposal = gt_boxes[best_gt_index.clamp(min=0)]
+        # Get best marching gt boxes for ALL proposals
+        # Even background proposals would have a gt box assigned to it
+        # Label will be used to ignore them later
+        matched_gt_boxes_for_proposals = gt_boxes[best_match_gt_idx.clamp(min=0)]
 
-        labels = gt_lables[best_gt_index.clamp(min=0)]
+        # Get class label for all proposals according to matching gt boxes
+        labels = gt_labels[best_match_gt_idx.clamp(min=0)]
         labels = labels.to(dtype=torch.int64)
 
-        background_proposal = best_gt_index == -1
-        labels[background_proposal] = 0
-        return labels, matched_gt_boxes_per_proposal
+        # Update background proposals to be of label 0(background)
+        labels[background_proposals] = 0
+
+        # Set all to be ignored anchor labels as -1(will be ignored)
+        labels[ignored_proposals] = -1
+
+        return labels, matched_gt_boxes_for_proposals
 
     def forward(self, feat, proposals, img_shape, target):
         if self.training and target is not None:
@@ -50,6 +74,6 @@ class ROIHead(nn.Module):
             gt_lables = target["labels"][0]
 
             # assign_target_to_proposal
-            labels, matched_gt_boxes_per_proposal = self.assign_target_to_proposal(
+            labels, matched_gt_boxes_per_proposal = self.assign_target_to_proposals(
                 proposals, gt_boxes, gt_lables
             )
